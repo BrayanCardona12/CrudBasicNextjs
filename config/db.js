1b4a5b92415686c65c52c871cccef2863fcad783
Brayan Cardona <brayancardona3778@gmail.com>
2023-04-09 13:53:39 -0500

    finish project

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './port'
 
 
 const pool =createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_NAME
+    host: 'containers-us-west-9.railway.app',
+    user: 'root',
+    password: 'dJukCCZ6YjulyDJ41Ihy',
+    port: 5518,
+    database: 'railway'
 })
 
 export {pool}
--- database/db.sql
+++ database/db.sql
@@ -2,4 +2,10 @@ CREATE productsdb if not exists;
 
 use productsdb; 
 
-
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(400) NOT NULL,
+    price DECIMAL,
+    createAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
